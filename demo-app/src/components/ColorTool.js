14a7d75706783ae4e74e159b73ea66358dc8f3a3
Eric Greene <eric@t4d.io>
2022-06-08 15:10:02 -0400

    add exercise 2

--- demo-app/src/components/ColorTool.js
+++ demo-app/src/components/ColorTool.js
@@ -1,14 +1,18 @@
 export const ColorTool = () => {
 
+  const colors = [
+    { id: 1, name: 'red', hexcode: 'ff0000' },
+    { id: 2, name: 'green', hexcode: '00ff00' },
+    { id: 3, name: 'blue', hexcode: '0000ff' },
+  ];
+
   return (
     <>
       <header>
         <h1>Color Tool</h1>
       </header>
       <ul>
-        <li>red</li>
-        <li>green</li>
-        <li>blue</li>
+        {colors.map(color => <li key={color.id}>{color.name}</li>)}
       </ul>
     </>
   );
